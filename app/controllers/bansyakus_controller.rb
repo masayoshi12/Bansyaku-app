a0798a0413baeb0c3533b854636ace9204e25786
masayoshi12 <miyabigi1222@gmail.com>
2021-02-08 12:41:02 +0900

    投稿画面作成

--- app/controllers/bansyakus_controller.rb
+++ app/controllers/bansyakus_controller.rb
@@ -1,4 +1,25 @@
 class BansyakusController < ApplicationController
   def index
+    @bansyakus = Bansyaku.includes(:user)
   end
+
+  def new
+    @bansyaku = Bansyaku.new
+  end
+  
+  def create
+    @bansyaku = Bansyaku.new(bansyaku_params)
+    if @bansyaku.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def bansyaku_params
+    params.require(:bansyaku).permit( :sake_name, :sake_id, :warimono_id, :onetumami_name, :onetumami_id, :onetumami_recipe, :twotumami_name, :twotumami_id, :twotumami_recipe, :user_comment, :image).merge(user_id: current_user.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "bansyakus#index"
+  resources :bansyakus, only: [:new, :create, :show, :edit, :update, :destroy]  
 end
