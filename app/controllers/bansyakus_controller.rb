d77795785f6d5f83ed32faa912f55a0af12e2099
masayoshi12 <miyabigi1222@gmail.com>
2021-02-08 15:43:13 +0900

    詳細画面表示

--- app/controllers/bansyakus_controller.rb
+++ app/controllers/bansyakus_controller.rb
@@ -1,4 +1,6 @@
 class BansyakusController < ApplicationController
+  before_action :set_bansyaku, except: [:index, :new, :create]
+
   def index
     @bansyakus = Bansyaku.includes(:user)
   end
@@ -15,11 +17,18 @@ class BansyakusController < ApplicationController
       render :new
     end
   end
+
+  def show
+  end
   
   private
   
   def bansyaku_params
     params.require(:bansyaku).permit( :sake_name, :sake_id, :warimono_id, :onetumami_name, :onetumami_id, :onetumami_recipe, :twotumami_name, :twotumami_id, :twotumami_recipe, :user_comment, :image).merge(user_id: current_user.id)
+  end
+
+  def set_bansyaku
+    @bansyaku = Bansyaku.find(params[:id])
   end
   
 end
